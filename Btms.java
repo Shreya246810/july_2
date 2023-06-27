12187b6ad35b4bc59c12ec98b066c4b1efb4ec25
Shreya246810 <shreya.xworkz@gmail.com>
2023-06-27 14:17:33 +0530

    This is first commit

---
+++ Btms.java
@@ -0,0 +1,31 @@
+class Btms{
+public static void main(String[] args){
+     String btmShop1 ="BTM Shop 1";	 
+     String btmShop2 ="BTM Shop 2";
+     String btmShop3 ="BTM Shop 3";
+     String btmShop4 ="BTM Shop 4";
+     String btmShop5 ="BTM ShOP 5";
+     String btmShop6 ="BTM Shop 6";
+	 String btmShop7 ="BTM Shop 7";
+	 String btmShop8 ="BTM Shop 8";
+     String btmShop9 ="BTM Shop 9";
+     String btmShop10 ="BTM Shop 10";
+  
+           
+     System.out.println("BtmShop 1: " + btmShop1");     
+     System.out.println("BtmShop 2: " + btmShop1");	 	 
+	 System.out.println("BtmShop 3: " + btmShop1");
+	 System.out.println("BtmShop 4: " + btmShop1");
+	 System.out.println("BtmShop 5: " + btmShop1");
+	 System.out.println("BtmShop 6: " + btmShop1");
+	 System.out.println("BtmShop 7: " + btmShop1");
+	 System.out.println("BtmShop 8: " + btmShop1");
+	 System.out.println("BtmShop 9: " + btmShop1");
+	 System.out.println("BtmShop 10: " + btmShop1");
+	 
+	 
+     }
+
+}	 
+	 
+							
